d7de7fa31c071e109aba7734f337de7f97ff43a6
mr <mr@192.168.0.15>
2018-09-30 21:14:18 +0200

    REFACTORING

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -48,65 +48,34 @@ public class GreetingController {
    @RequestMapping("/tree")
    public String sayHello() throws JsonParseException, JsonMappingException, IOException, ParseException, URISyntaxException, Exception {
 	   
-	   ///////////// prosty rest
-	   final String uri = "https://jsonplaceholder.typicode.com/todos/1";
-
-	    RestTemplate restTemplate = new RestTemplate();
-	    String result = restTemplate.getForObject(uri, String.class);
-
-	    System.out.println(result);
-	    
-	    			JSONParser jsonParser = new JSONParser();
-	                JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
-
-	                // get a String from the JSON object
-	                String firstName = (String) jsonObject.get("title");
-	                System.out.println("The first name is title: " + firstName);
+/////////// WYKONAJ STRZAL REST
 	   
-	   //String firstName = "MArR";
-	                Connection conn = null;
+				   restCALL rest = new restCALL();
+				   String retFirtsName = rest.makeAcall();
+				   
+				   
+/////////// OBROB WYNIKI
+				   
+	               String resulte = retFirtsName + " || jest!!!  ||  "+System.currentTimeMillis();
+	               resulte = resulte + "\n";
 	                
-	                String resulte = firstName + " || jest!!!  ||  "+System.currentTimeMillis();
-	                resulte = resulte + "\n";
-	                Properties props = new Properties();
-	                //props.setProperty("ssl","true");
-
-	                String usernamee = "qgrpxxclzflgqb";
-	                String passwordd = "c80bc47f7c64ea617e553ab2825da39d509bbdc5bb283be7a3579f7a18aeb3d3";
-	                String dbUrll = "jdbc:postgresql://ec2-54-247-101-205.eu-west-1.compute.amazonaws.com:5432/dc0l2m58psg7rg";
-
-	                try {
-						conn = DriverManager.getConnection(dbUrll, usernamee, passwordd);
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						System.out.println(e1.getMessage());
-						e1.printStackTrace();
-						
-					}
+	               
+/////////// POBIERZ POLACZENIE DO BAZY
 	                
-	  ///////////// podlaczenie pod DB Postgre SQL
+	                getDBConn getDB = new getDBConn();
+	                Connection conn = getDB.getConn();
+	                System.out.println("Connected to the PostgreSQL server successfully.");   
 	                
-	                //conn = DriverManager.getConnection(url, user, password);
-					//conn = DriverManager.getConnection(url, props);
-					System.out.println("Connected to the PostgreSQL server successfully.");   
 	                
-	                String SQL = "SELECT count(*) FROM Persons";
-	                int count = 0;
-	         
-	                System.out.println("Przed select");
+/////////// WYKONAJ ZAPYTANIE
 	                
-	                try (
-	                        Statement stmt = conn.createStatement();
-	                        ResultSet rs = stmt.executeQuery(SQL)) {
-	                    rs.next();
-	                    count = rs.getInt(1);
-	                } catch (SQLException ex) {
-	                    System.out.println(ex.getMessage());
-	                }
-	                
-	                System.out.println("Po select");
-	                
-	                resulte = resulte + "Persons licznik::" + count;
+					makeDBStmt DB = new makeDBStmt();
+					int count = DB.makeQuery(conn);
+					
+					
+/////////// SKLEJ WYNIKI
+					
+	                resulte = resulte + "Persons licznik 2::" + count;
 	   
       return resulte; //result;
    }
